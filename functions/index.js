e617646a97853eb71be54e1d6526dc98213a7407
ufoo68 <mitarou.yuutarou@gmail.com>
2019-05-11 13:06:03 +0900

    Intentの処理をswitch文に変更

--- functions/index.js
+++ functions/index.js
@@ -47,18 +47,21 @@ const clovaSkillHandler = clova.Client
     .onIntentRequest(async responseHelper => {
         const intent = responseHelper.getIntentName();
         const sessionId = responseHelper.getSessionId();
+        const slots = responseHelper.getSlots();
 
         console.log('Intent:' + intent);
 
+        let speech = {
+            lang: 'ja',
+            type: 'PlainText',
+            value:  `まだ対応していないカラーコード、または抵抗値です。`
+        }
+
+        switch(intent) {
         // カラーコード -> 抵抗値の処理
-        if(intent === 'FindValueByColorcodeIntent'){
-            const slots = responseHelper.getSlots();
+        case 'FindValueByColorcodeIntent':
             const colorcode = String(slots.colorcode);
-            let speech = {
-                lang: 'ja',
-                type: 'PlainText',
-                value:  `まだ対応していないカラーコードです。`
-            }
+            
             // 通常の４つのカラーコードの並びの抵抗
             if (colorcode.length === 4) {
                 const firstValue = colorcodes[colorcode[0]] //1番目のカラーコード
@@ -87,20 +90,10 @@ const clovaSkillHandler = clova.Client
                 console.log('Registor value:', registorValue(thirdValue));
                 speech.value = `${colorcode}の抵抗値は${registorValue(thirdValue)}Ωです。`;
             }
-            responseHelper.setSimpleSpeech(speech);
-            responseHelper.setSimpleSpeech(
-                clova.SpeechBuilder.createSpeechText('抵抗器のカラーコード、またはカラーコードに変換したい抵抗値を教えてください。'), true
-            );
-        }
+            break;
 
         // 抵抗値 -> カラーコードの処理
-        else if (intent === 'FindColorcodeByValueIntent') {
-            const slots = responseHelper.getSlots();
-            let speech = {
-                lang: 'ja',
-                type: 'PlainText',
-                value:  `まだ対応していない数字です。`
-            }
+        case 'FindColorcodeByValueIntent':
             const registorValue = String(slots.number);
             console.log('slots:', registorValue)
             // 半角数字のとき(漢数字は自動的に半角英数字に変換される)
@@ -117,38 +110,23 @@ const clovaSkillHandler = clova.Client
                 const colorcode = `${firstColor}${secondColor}${thirdColor}`;
                 speech.value = `${registorValue}Ωのカラーコードは${colorcode}金です。`
             }
-            responseHelper.setSimpleSpeech(speech);
-            responseHelper.setSimpleSpeech(
-                clova.SpeechBuilder.createSpeechText('抵抗器のカラーコード、またはカラーコードに変換したい抵抗値を教えてください。'), true
-            );
-        }
+            break;
+
         // カラーコードの覚え方
-        else if (intent === 'HowToMemorizeColorcode') {
-            const slots = responseHelper.getSlots();
-            let speech = {
-                lang: 'ja',
-                type: 'PlainText',
-                value:  `対応していない色です。`
-            }
+        case 'HowToMemorizeColorcode':
             const mnemonic = howToMemorize[String(slots.color)];
             speech.value = `${mnemonic}。と覚えましょう。`
-            responseHelper.setSimpleSpeech(speech);
-            responseHelper.setSimpleSpeech(
-                clova.SpeechBuilder.createSpeechText('抵抗器のカラーコード、またはカラーコードに変換したい抵抗値を教えてください。'), true
-            );
+            break;
+        
+        //Intentをうまく受け取れなかったとき
+        default:
+            speech.value = `もう一度お願いします。`;
+            break;
         }
-
-        else {
-            let speech = {
-                lang: 'ja',
-                type: 'PlainText',
-                value:  `もう一度お願いします。`
-            }
-            responseHelper.setSimpleSpeech(speech);
-            responseHelper.setSimpleSpeech(
-                clova.SpeechBuilder.createSpeechText('抵抗器のカラーコード、またはカラーコードに変換したい抵抗値を教えてください。'), true
-            );
-        }
+        responseHelper.setSimpleSpeech(speech);
+        responseHelper.setSimpleSpeech(
+            clova.SpeechBuilder.createSpeechText('抵抗器のカラーコード、またはカラーコードに変換したい抵抗値を教えてください。'), true
+        );
     })
 
     //終了時
